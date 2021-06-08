6dee8d4401f7ccd7e748b53715646f80ce881519
Navpreet Rajput <84348431+NavpreetRajput@users.noreply.github.com>
2021-06-08 11:25:54 -0400

    Code Formatting

--- src/schoolManagementSystem/Schools.java
+++ src/schoolManagementSystem/Schools.java
@@ -1,105 +1,110 @@
 package schoolManagementSystem;
+
 import java.util.ArrayList;
 
 /* This class is created to keep records of all students, teachers, total Money earned by the School, total  money spent by the school*/
 public class Schools {
-    
-     
+
     private ArrayList<Teacher> teachers;
     private ArrayList<Student> students;
     private static double totalMoneyEarned;
-    private static double totalMoneySpent; 
-    
-/**
- * new school object is created
- * @param teachers list of teachers in the school
- * @param students list of students in the school
- */
+    private static double totalMoneySpent;
+
+    /**
+     * new school object is created
+     * 
+     * @param teachers list of teachers in the school
+     * @param students list of students in the school
+     */
     public Schools(ArrayList<Teacher> teachers, ArrayList<Student> students)
-   
+
     {
-        
-        this.teachers=teachers;
-        this.students=students;
-        totalMoneyEarned=0;
-        totalMoneySpent=0;
+
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent = 0;
     }
 
     /**
      * Adds a teacher to the school
+     * 
      * @param teachers the teacher to be added
      */
-    public void addTeachers(Teacher teachers){
+    public void addTeachers(Teacher teachers) {
         teachers.add(teachers);
-        
+
     }
+
     /**
      * 
      * @return returns the list of the teachers
      */
-    public ArrayList<Teacher> getTeachers(){
+    public ArrayList<Teacher> getTeachers() {
         return teachers;
 
     }
 
     /**
      * Adds a student to the school
+     * 
      * @param students the student to be added
      */
-    public void addStudents(Student students){
+    public void addStudents(Student students) {
 
         students.add(students);
-        //System.out.println("New student "+students+)
-       
+        // System.out.println("New student "+students+)
+
     }
 
     /**
      * 
      * @return returns the list of the student
      */
-    public ArrayList<Student> getStudents(){
+    public ArrayList<Student> getStudents() {
         return students;
     }
 
     /**
-     * Update the total money spent by the school by keep subtracting the totalMoneySpent from the totalMoneyEarned
+     * Update the total money spent by the school by keep subtracting the
+     * totalMoneySpent from the totalMoneyEarned
+     * 
      * @param totalMoneySpent is the money spent by the school
      */
-    public static void updateTotalMoneySpent(double totalMoneySpents){
-        totalMoneySpent+=totalMoneySpents; 
+    public static void updateTotalMoneySpent(double totalMoneySpents) {
+        totalMoneySpent += totalMoneySpents;
     }
 
     /**
      * 
      * @return returns the total money earned by the school
      */
-    public Double getTotalMoneySpent(){
+    public Double getTotalMoneySpent() {
         return totalMoneySpent;
 
     }
-    
+
     /**
      * updates the total money earned by keep adding it into the totalMoneyEarned
+     * 
      * @param totalMoneyEarn is the money earned by the school
      */
-    public static void updateTotalMoneyEarned(double totalMoneyEarn ){
-        totalMoneyEarned+=totalMoneyEarn;
+    public static void updateTotalMoneyEarned(double totalMoneyEarn) {
+        totalMoneyEarned += totalMoneyEarn;
     }
 
     /**
      * 
      * @return returns the total money earned by the school
      */
-    public Double getTotalMoneyEarned(){
-        return  totalMoneyEarned;
+    public Double getTotalMoneyEarned() {
+        return totalMoneyEarned;
     }
-   
 
-public void salaries(){
-    
+    public void salaries() {
 
+    }
 }
-    }
 
-
-
+    
+    
